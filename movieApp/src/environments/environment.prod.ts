a45d9f825b4b90092d37994a55e66978caeb4ebb
KiarieLinus <kiarielinus@gmail.com>
2023-05-03 18:35:39 +0300

    Ionic base code and Android version

---
+++ movieApp/src/environments/environment.prod.ts
@@ -0,0 +1,6 @@
+export const environment = {
+  production: true,
+  apiKey: '99e58387ce983a948bdc2d18fd7d2530',
+  baseUrl: 'https://api.themoviedb.org/3',
+  images: 'https://images.tmdb.org/t/p',
+};
